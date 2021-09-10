1b150405e9570a81ebcb4798c2ce76d9c3559e47
DivDE <divde@musicociel.fr>
2021-09-10 14:03:15 +0200

    Changing configuration of playwright

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -6,6 +6,13 @@ const config: PlaywrightTestConfig = {
   retries: 5,
   forbidOnly: !!process.env.CI,
   outputDir: join(__dirname, "test-results"),
+  webServer: process.env.TEST_MUSICOCIEL_URL
+    ? undefined
+    : {
+        command: "npm start",
+        port: 8081,
+        reuseExistingServer: !process.env.CI
+      },
   use: {
     baseURL: process.env.TEST_MUSICOCIEL_URL || "http://127.0.0.1:8081",
     viewport: { width: 1280, height: 720 },
@@ -13,6 +20,20 @@ const config: PlaywrightTestConfig = {
     screenshot: "only-on-failure",
     video: "retain-on-failure",
     trace: "retain-on-failure"
-  }
+  },
+  projects: [
+    {
+      name: "Chromium",
+      use: {
+        browserName: "chromium"
+      }
+    },
+    {
+      name: "Firefox",
+      use: {
+        browserName: "firefox"
+      }
+    }
+  ]
 };
 export default config;
