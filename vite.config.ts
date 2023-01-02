2c049a0149b883aed7331bdd720d011f1c218d0f
DivDE <divde@musicociel.fr>
2023-01-02 19:18:19 +0100

    Update logo (and all variations using https://realfavicongenerator.net)

--- vite.config.ts
+++ vite.config.ts
@@ -20,26 +20,18 @@ export default defineConfig({
         short_name: "Musicociel",
         display: "standalone",
         start_url: ".",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
         icons: [
           {
-            src: "favicon.ico",
-            sizes: "16x16",
-            type: "image/x-icon"
-          },
-          {
-            src: "favicon144.png",
-            sizes: "144x144",
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "favicon512.png",
+            src: "android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png"
-          },
-          {
-            src: "favicon.svg",
-            sizes: "600x600",
-            type: "image/svg+xml"
           }
         ],
         file_handlers: [
