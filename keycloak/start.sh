3fe5d6cd0bd9661d67a5dae5e6857175aae374e8
DivDE <divde@musicociel.fr>
2023-02-08 21:27:11 +0100

    Updating dependencies

--- keycloak/start.sh
+++ keycloak/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-podman run --rm -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v ./entrypoint.sh:/entrypoint.sh --entrypoint /entrypoint.sh quay.io/keycloak/keycloak:20.0.2 start-dev 2>&1 | tee keycloak.log
+podman run --rm -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin -v ./entrypoint.sh:/entrypoint.sh --entrypoint /entrypoint.sh quay.io/keycloak/keycloak:20.0.3 start-dev 2>&1 | tee keycloak.log
